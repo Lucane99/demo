92e8a1f6511ce3c151b34d08c4b253225ce4a4cb
Lucane99 <lucaneryr123@gmail.com>
2023-03-29 08:35:45 +0545

    changes

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+// variable declare and assign
+
+
+
+
+let personName = 'hari shyam';
+const pers = 'lio jee';
+const age = 90;
+// pers='liop';
+console.log(pers);
+const newName = personNamee + '' + pers;
+console.log(newName);
+
+
+
+const mio = `${personName} ${pers} ${newName} ${age}`;
+console.log(nio);
+
+
+
+
+
+console.log(personName);
+console.log(typeof (personName));
+console.log(personName.toLocaleUpperCase());
+console.log(personName.length);
+console.log(personName.trim().length);
+console.log(personName.trim().length);
+
+
+
+
+
+
